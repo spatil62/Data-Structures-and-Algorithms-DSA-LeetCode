edf6b7036e3389d9cf63a59da3204e5c9f5ccdcd
sneha <spatil62@hawk.iit.edu>
2025-05-23 11:25:04 -0500

    Added Merge k Sorted Lists Java solution under Linked_List

---
+++ Linked_List/Merge_K_Sorted_Lists.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ 
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        if (lists.length == 0) {
+            return null;
+        }
+
+        while (lists.length > 1) {
+            //VIMP//hold the reference of listNode 
+            List<ListNode> mergedLists = new ArrayList<>();
+            for (int i = 0; i < lists.length; i += 2) {
+                ListNode l1 = lists[i];
+                ListNode l2 = (i + 1 < lists.length) ? lists[i + 1] : null;
+                mergedLists.add(mergeList(l1, l2));
+            }
+            //lists = mergedLists.toArray(new ListNode[mergedLists.size()]);
+            lists = mergedLists.toArray(new ListNode[0]);
+        }
+
+        return lists[0];
+    }
+
+    private ListNode mergeList(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        // Attach the remaining nodes
+        if (l1 != null) {
+            tail.next = l1;
+        } else if (l2 != null) {
+            tail.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
