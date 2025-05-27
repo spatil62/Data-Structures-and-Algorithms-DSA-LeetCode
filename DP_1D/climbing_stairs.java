d71370e503f51a0f3e3d9fd696511ffabed1ea4e
sneha <spatil62@hawk.iit.edu>
2025-05-27 11:30:50 -0500

    Added Climbing Stairs solution in Java

---
+++ DP_1D/climbing_stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public int climbStairs(int n) {
+
+        if(n==1)
+        return 1;
+
+        if(n==2)
+        return 2;
+
+        if(map.containsKey(n))
+        return map.get(n);
+
+        int result = climbStairs(n-1)+climbStairs(n-2);
+        map.put(n,result);
+
+        return result;
+        
+    }
+}
